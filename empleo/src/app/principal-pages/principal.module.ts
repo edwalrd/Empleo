67729758268a626520cc221a5cdf569fd1e08a8a
unknown <edwallg@gmail.com>
2020-08-29 17:20:21 -0400

    pagina principar casi lista

--- empleo/src/app/principal-pages/principal.module.ts
+++ empleo/src/app/principal-pages/principal.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmpleoComponent } from './empleo/empleo.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './plantilla/header/header.component';
+import { FooterComponent } from './plantilla/footer/footer.component';
+import { BuscadorComponent } from './buscador/buscador.component';
 
 
 
 @NgModule({
-  declarations: [EmpleoComponent],
+  declarations: [EmpleoComponent, HeaderComponent, FooterComponent, BuscadorComponent],
   exports: [EmpleoComponent],
   imports: [
     CommonModule,
